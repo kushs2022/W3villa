b048b93a3ad45c3fc638ed332389fd23440aa79e
kushs2022 <65161538+kushs2022@users.noreply.github.com>
2021-03-19 01:34:48 +0530

    Add files via upload

---
+++ js/questions-1.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In general, the index of the first element in an array is",
+    answer: "0",
+    options: [
+      "0",
+      "-1",
+      "2",
+      "1"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are the advantages of arrays?",
+    answer: "Easier to store elements of same data type",
+    options: [
+      "Objects of mixed data types can be stored",
+      "Elements in an array cannot be sorted",
+      "Index of first element of an array is 1",
+      "Easier to store elements of same data type"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following concepts make extensive use of arrays?",
+    answer: "Spatial locality",
+    options: [
+      "Binary trees",
+      "Scheduling of processes",
+      "Caching",
+      "Spatial locality"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When does the ArrayIndexOutOfBoundsException occur?",
+    answer: "Run-time",
+    options: [
+      " Compile-time",
+      "Run-time",
+      "Not an error",
+      "Not an exception at all"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you initialize an array in C?",
+    answer: "int arr[3] = {1,2,3};",
+    options: [
+      " int arr[3] = (1,2,3);",
+      " int arr(3) = {1,2,3};",
+      "int arr[3] = {1,2,3};",
+      "int arr(3) = (1,2,3);"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/questions-2.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following statements for a simple graph is correct?",
+    answer: "Every path is a trail",
+    options: [
+      "Every path is a trail",
+      " Every trail is a path",
+      "Every trail is a path as well as every path is a trail",
+      "Path and trail have no relation"
+    ]
+  },
+    {
+    numb: 1,
+    question: "A connected planar graph having 6 vertices, 7 edges contains _____________ regions?",
+    answer: "3",
+    options: [
+      "15",
+      "3",
+      "1",
+      "11"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following ways can be used to represent a graph?",
+    answer: "Adjacency List, Adjacency Matrix as well as Incidence Matrix",
+    options: [
+      "Adjacency List and Adjacency Matrix",
+      " Incidence Matrix",
+      "Adjacency List, Adjacency Matrix as well as Incidence Matrix",
+      "No way to represent"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the maximum number of edges in a bipartite graph having 10 vertices?",
+    answer: "21",
+    options: [
+      "24",
+      "21",
+      "25",
+      "16"
+    ]
+  },
+    {
+    numb: 5,
+    question: " A graph with all vertices having equal degree is known as a",
+    answer: "Regular Graph",
+    options: [
+      "Multi Graph",
+      "Regular Graph",
+      "Simple Graph",
+      "Complete Graph"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
